ea598c8df6ff4cf1276837389750cafa2f8043b8
burobo <boronobu@gmail.com>
2020-11-10 13:41:35 +0900

    subtractTestを実装

--- src/test/groovy/jp/co/biglobe/warikan/domain/amount/AmountTest.groovy
+++ src/test/groovy/jp/co/biglobe/warikan/domain/amount/AmountTest.groovy
@@ -11,4 +11,17 @@ class AmountTest extends Specification {
         then:
             amount1.amount() == 25
     }
+
+    def subtractTest() {
+        when:
+            Amount amount1 = new Amount(a)
+            Amount amount2 = new Amount(b)
+            amount1.subtract(amount2)
+        then:
+            amount1.amount() == expected
+        where:
+            a  | b  | expected
+            15 | 10 | 5
+            10 | 15 | -5
+    }
 }
