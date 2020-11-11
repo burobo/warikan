3022465fcb0b8cc794e62d583205613619dda5db
burobo <boronobu@gmail.com>
2020-11-11 14:39:49 +0900

    テストコード内でアサーションにamountメソッドを使用しないようにした

--- src/test/groovy/jp/co/biglobe/warikan/domain/amount/AmountTest.groovy
+++ src/test/groovy/jp/co/biglobe/warikan/domain/amount/AmountTest.groovy
@@ -5,27 +5,28 @@ import spock.lang.Specification
 class AmountTest extends Specification {
     def addTest() {
         when:
-            Amount amount1 = new Amount(10)
-            Amount amount2 = new Amount(15)
-            Amount amount3 = amount1.add(amount2)
+        Amount amount1 = new Amount(10)
+        Amount amount2 = new Amount(15)
+        Amount amount3 = amount1.add(amount2)
         then:
-            amount1.amount() == 10
-            amount2.amount() == 15
-            amount3.amount() == 25
+        amount1.amount == 10
+        amount2.amount == 15
+        amount3.amount == 25
     }
 
     def subtractTest() {
         when:
-            Amount amount1 = new Amount(a)
-            Amount amount2 = new Amount(b)
-            Amount amount3 = amount1.subtract(amount2)
+        Amount amount1 = new Amount(a)
+        Amount amount2 = new Amount(b)
+        Amount amount3 = amount1.subtract(amount2)
         then:
-            amount1.amount() == a
-            amount2.amount() == b
-            amount3.amount() == expected
+        amount1.amount == a
+        amount2.amount == b
+        amount3.amount == expected
         where:
-            a  | b  | expected
-            15 | 10 | 5
-            10 | 15 | -5
+        a  | b  | expected
+        15 | 10 | 5
+        10 | 15 | -5
     }
+
 }
