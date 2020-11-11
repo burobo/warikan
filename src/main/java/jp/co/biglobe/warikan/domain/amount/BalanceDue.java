9c87b57110f85e7c246f43f491d22b76541162de
burobo <boronobu@gmail.com>
2020-11-11 15:11:14 +0900

    BalanceDueクラスを作成した

---
+++ src/main/java/jp/co/biglobe/warikan/domain/amount/BalanceDue.java
@@ -0,0 +1,10 @@
+package jp.co.biglobe.warikan.domain.amount;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public class BalanceDue {
+    @Getter
+    Amount amount;
+}
